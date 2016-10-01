72f9cd0ffbdf8b0e63788762861f69884087452a
Nebuta <nebuta.office@gmail.com>
2016-10-01 15:03:43 +0900

    Use polymorphic variants for interface

---
+++ compile.sh
@@ -0,0 +1,1 @@
+./node_modules/bs-platform/bin/bsc.exe -I lib/ocaml -c -bs-main lib/ocaml/jquery.ml -bs-package-name bucklescript-jquery -bs-package-output lib/js
--- lib/ocaml/jquery.ml
+++ lib/ocaml/jquery.ml
@@ -6,53 +6,120 @@ type ('a,'b) attr_func = jquery -> int -> 'a -> 'b [@bs.this];;
 type attr_func_str = (string,string) attr_func;;
 type js_coord = < top : int ; left : int > Js.t
 
-external jquery : string -> jquery = "" [@@bs.module]
-external jquery_ : jquery =  "jquery" [@@bs.module]
+type t;;
+external jquery : string -> jquery = "" [@@bs.module "jquery", "$"]
+external jquery_ : jquery =  "jquery" [@@bs.module "jquery","$"]
 external jquery' : jquery -> jquery = "jquery" [@@bs.module]
 
-type 'a attr_type = Str of string * string
-	| Kv of 'a Js.t
-	| Func of (string,string) attr_func;;
+let jquery = jquery;;
+let jquery_ = jquery_;;
+let jquery' = jquery';;
+
 
 (* Attributes *)
 external addClass : string -> jquery = "addClass" [@@bs.send.pipe: jquery]
 external addClass' : (string,string) attr_func -> jquery = "addClass" [@@bs.send.pipe: jquery]
+let addClass at jq =
+	match at with
+		`str s -> addClass s jq
+		| `func f -> addClass' f jq;;
+
 external attr_get : string -> string = "attr" [@@bs.send.pipe: jquery]
+let attr_get = attr_get;;
 
 external attr_ : string -> string -> jquery = "attr" [@@bs.send.pipe: jquery]
 external attr__ : 'a Js.t -> jquery = "attr" [@@bs.send.pipe: jquery]
 external attr' : (string,string) attr_func -> jquery = "attr" [@@bs.send.pipe: jquery]
 
-let attr (at : 'a attr_type) (jq : jquery) : jquery =
+let attr at (jq : jquery) : jquery =
 	match at with
-		Str (k,v) -> attr_ k v jq;
-		| Kv obj -> attr__ obj jq
-		| Func f -> attr' f jq;;
+		`kv (k,v) -> attr_ k v jq;
+		| `map obj -> attr__ obj jq
+		| `func f -> attr' f jq;;
+
 
 external hasClass : string -> Js.boolean = "hasClass" [@@bs.send.pipe: jquery]
+let hasClass = hasClass;;
+
 external html_get : string = "html" [@@bs.send.pipe: jquery]
 external html : string -> jquery = "html" [@@bs.send.pipe: jquery]
 external html' : (string,string) attr_func -> jquery = "html" [@@bs.send.pipe: jquery]
-external prop : string -> string = "prop" [@@bs.send.pipe: jquery]
-external prop_bool : string -> Js.boolean = "prop" [@@bs.send.pipe: jquery]
+
+let html_get = html_get;;
+let html at (jq : jquery) : jquery =
+	match at with
+		`str s -> html s jq;
+		| `func f -> html' f jq;;
+
+
+external prop_get : string -> 'a Js.t = "prop" [@@bs.send.pipe: jquery]
+
+(* external prop_get_bool : string -> Js.boolean = "prop" [@@bs.send.pipe: jquery] *)
 external prop_ : string -> string -> jquery = "prop" [@@bs.send.pipe: jquery]
 external prop__ : 'a Js.t -> jquery = "prop" [@@bs.send.pipe: jquery]
 external prop' : (string,string) attr_func -> jquery = "prop" [@@bs.send.pipe: jquery]
+
+let prop_get (type t) (k : string) : [`str of t | `bool of t | `error] =
+	let p = prop_get k in
+	let ty, v = Js.Types.reify_type p in
+	match (ty : t Js.Types.t) with
+	| Js.Types.String -> `str v
+	| Js.Types.Boolean -> `bool v
+	| _ -> `error;;
+
+let prop at (jq : jquery) : jquery =
+	match at with
+		| `kv (k,v) -> prop_ k v jq
+		| `map obj -> prop__ obj jq
+		| `func f -> prop' f jq;;
+
 external removeAttr : string -> jquery = "removeAttr" [@@bs.send.pipe: jquery]
+let removeAttr = removeAttr;;
+
 external removeClass : string -> jquery = "removeClass" [@@bs.send.pipe: jquery]
 external removeClass_ : jquery = "removeClass" [@@bs.send.pipe: jquery]
 external removeClass' : (string,string) attr_func -> jquery = "removeClass" [@@bs.send.pipe: jquery]
+
+let removeClass at jq =
+	match at with
+		| `void -> removeClass_ jq
+		| `str s -> removeClass s jq
+		| `func f -> removeClass' f jq;;
+
 external removeProp : string -> jquery = "removeProp" [@@bs.send.pipe: jquery]
+
+let removeProp = removeProp;;
+
 external toggleClass : string -> jquery = "toggleClass" [@@bs.send.pipe: jquery]
-external toggleClass_ : string -> string -> jquery = "toggleClass" [@@bs.send.pipe: jquery]
-external toggleClass' : (string,string) attr_func -> Js.boolean -> jquery = "toggleClass" [@@bs.send.pipe: jquery]
-external value_get : string = "val" [@@bs.send.pipe: jquery]
-external value : string -> jquery = "val" [@@bs.send.pipe: jquery]
-external value' : (string,string) attr_func -> jquery = "val" [@@bs.send.pipe: jquery]
+external toggleClass_ : string -> Js.boolean -> jquery = "toggleClass" [@@bs.send.pipe: jquery]
+external toggleClass' : (string,string) attr_func -> jquery = "toggleClass" [@@bs.send.pipe: jquery]
+external toggleClass'' : (string,string) attr_func -> Js.boolean -> jquery = "toggleClass" [@@bs.send.pipe: jquery]
+
+let to_js_bool b = if b then Js.true_ else Js.false_
+
+let toggleClass at jq =
+	match at with
+		`str s -> toggleClass s jq
+		| `strs ss -> toggleClass (String.concat " " ss) jq
+		| `strb (s,b) -> toggleClass_ s (to_js_bool b) jq
+		| `strs_b (ss,b) -> toggleClass_ (String.concat " " ss) (to_js_bool b) jq
+		| `func f -> toggleClass' f jq
+		| `func_b (f,b) -> toggleClass'' f b jq
+
+external val_get : string = "val" [@@bs.send.pipe: jquery]
+let val_get = val_get
+
+external val_ : string -> jquery = "val" [@@bs.send.pipe: jquery]
+external val_' : (string,string) attr_func -> jquery = "val" [@@bs.send.pipe: jquery]
+let val_ at jq =
+	match at with
+		`str s -> val_ s jq
+		| `func f -> val_' f jq
 
 (* CSS *)
 external css_get : string -> string = "css" [@@bs.send.pipe: jquery]
 external css_get' : string array -> string = "css" [@@bs.send.pipe: jquery]
+
 external css : string -> string -> jquery = "css" [@@bs.send.pipe: jquery]
 external css' : 'a Js.t -> jquery = "css" [@@bs.send.pipe: jquery]
 external css'' : string -> attr_func_str -> jquery = "css" [@@bs.send.pipe: jquery]
